6e5c3fd77675c47b8129bbfd401cdd0771123109
Pathum Liyanagama <pathumpml.dev@gmail.com>
2026-02-06 07:36:50 +0800

    Fetch categories

--- src/components/Products/ProductsSidebar.jsx
+++ src/components/Products/ProductsSidebar.jsx
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ProductsSidebar.css";
 import LinkWithIcon from "../Navbar/LinkWithIcon";
-import rocket from "../../assets/rocket.png";
+import apiClient from "../../utils/api-client";
 
 const ProductsSidebar = () => {
+  const [categories, setCategories] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    apiClient
+      .get("/category")
+      .then((response) => {
+        console.log(response);
+        setCategories(response.data);
+      })
+      .catch((err) => {
+        setError(err.message);
+        console.error("Error fetching categories:", err);
+      });
+  }, []);
+
   return (
     <aside className="products_sidebar">
       <h2>Category</h2>
       <div className="category_links">
-        <LinkWithIcon
-          title="Electronics"
-          link="products?category=electronics"
-          icon={rocket}
-          sidebar={true}
-        />
+        {error && <em className="form_error">{error}</em>}
+        {categories.map((category) => (
+          <LinkWithIcon
+            key={category._id}
+            id={category._id}
+            title={category.name}
+            link={`products?category=${category.name}`}
+            icon={`http://localhost:5001/category/${category.image}`}
+            sidebar={true}
+          />
+        ))}
       </div>
     </aside>
   );
